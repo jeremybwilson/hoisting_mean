580b3bb714c612d43aa659ee9715447cdbf29dcc
Jeremy Wilson <jeremybwilson@gmail.com>
2018-10-27 17:07:04 -0700

    renamed files and included in html file

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 // Objectives:
 // Practice reading JavaScript the same way as the interpreter reads it.
 // Rewrite the code the way it would be seen by the interpreter and predict the output.  An example is shown here:
+
 // GIVEN
 // console.log(example);
 // var example = "I'm the example!";
 // AFTER HOISTING BY THE INTERPRETER
-// var example;
+
+var example;
 // console.log(example); // logs undefined
-// example = "I'm the example!";
+example = "I'm the example!";
 
 // Note
 // Run the same code as above with ES6 syntax and compare your results:
@@ -16,43 +18,46 @@
 // console.log(example);
 // let example = "I'm the example!";
 
-
-let example;
+let example1;
 // console.log(`Here is the value: ${example}`);
-example = "I'm the example!";
+example1 = "I'm the example!";
 
 // 1
-console.log(hello);
+// console.log(hello);
 var hello = 'Hello World';
 
 // returns hello is undefined
 
 // Question 2
-var needle = 'haystack';
-test();
-function test() {
-    var needle = 'magnet';
+const needle = 'haystack';
+// test();
+const test = () => {
+    const needle = 'magnet';
     console.log(needle);
+    console.log(`${needle}`);
 }
+// test();
 // returns magnet
 
 // Question 3
 var jeremy = 'super cool';
-function print() {
-    jeremy = 'only okay';
-    console.log(jeremy);
+// debugger;
+function print(){
+    const jeremy = 'only okay';
+    console.log(`${jeremy}`);
 }
-console.log(jeremy);
+// print();
+// console.log(`${jeremy}`);
 
 // returns 'super cool'
 
 // Question 4
 var food = 'chicken';
-console.log(food);
-eat();
+// console.log(`${food}`);
+// eat();
 function eat() {
     food = 'half-chicken';
-    console.log(food);
+    console.log(`${food}`);
     var food = 'gone';
 }
 
@@ -61,14 +66,14 @@ function eat() {
 
 // Question 5
 // mean();
-// console.log(food);
-var mean = function () {
+// console.log(`${food}`);
+var mean = () => {
     food = "chicken";
-    console.log(food);
+    console.log(`${food}`);
     var food = "fish";
-    console.log(food);
+    console.log(`${food}`);
 }
-// console.log(food);
+// console.log(`${food}`);
 
 // returns an error => TypeError: mean is not a function, variable mean got hoisted to the top before the function was assigned
 
@@ -76,28 +81,34 @@ var mean = function () {
 // Question 6
 // console.log(genre);
 var genre = "disco";
-rewind();
+// rewind();
 function rewind() {
     genre = "rock";
-    console.log(genre);
+    console.log(`${genre}`);
     var genre = "r&b";
-    console.log(genre);
+    console.log(`${genre}`);
 }
-console.log(genre);
+// const rewind = () => {
+//     genre = "rock";
+//     console.log(`${genre}`);
+//     var genre = "r&b";
+//     console.log(`${genre}`);
+// }
+// console.log(`${genre}`);
 
 // returns undefined, then rock, r&b, then disco
 
 // Question 7
 dojo = "san jose";
-console.log(dojo);
-learn();
+// console.log(`${dojo}`);
+// learn();
 function learn() {
     dojo = "seattle";
-    console.log(dojo);
+    console.log(`${dojo}`);
     var dojo = "burbank";
-    console.log(dojo);
+    console.log(`${dojo}`);
 }
-console.log(dojo);
+// console.log(`${dojo}`);
 
 // returns san jose, then seattle, then burbank, then san jose again
 
@@ -105,7 +116,7 @@ console.log(dojo);
 console.log(makeDojo("Chicago", 65));
 console.log(makeDojo("Berkeley", 0));
 function makeDojo(name, students) {
-    const dojo = {};
+    let dojo = {};
     dojo.name = name;
     dojo.students = students;
     if (dojo.students > 50) {
